46c6698b46ebb3c929022319c99dab80f6d754e7
Mwero Abdalla <mwerothewebmaker@gmail.com>
2023-08-22 11:12:50 +0300

    fix : bug-fix in vite-config

--- server/src/server.js
+++ server/src/server.js
@@ -12,7 +12,8 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname,'public')));
+// app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'html')));
 app.set('view engine', 'html');
 
 
--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '',
+//   root: '.',
+//   build: {
+//     outDir: './server/src/public'
+//   }
+// })
+
 export default defineConfig({
   plugins: [react()],
   base: '',
   root: '.',
   build: {
-    outDir: './server/src/public'
+    outDir: './dist'
   }
 })
